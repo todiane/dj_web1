225577ccce5a924054c2b71bb87601e3dc8ba837
Diane Corriette <dcorriette@gmail.com>
2024-05-02 15:31:27 +0100

    Replace hardcoded information in html files with dynamic values obtained from class field

--- app/views.py
+++ app/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 
+from app.models import (
+    GeneralInfo,
+    Service,
+)
 
-# Create your views here.
+
 def index(request):
-  return render(request, 'index.html', {})
+
+    general_info = GeneralInfo.objects.first() # None
+
+    services = Service.objects.all()
+
+    default_value = ""
+
+    context = {
+        "company_name": getattr(general_info, "company_name", default_value),
+        "location": getattr(general_info, "location", default_value),
+        "email": getattr(general_info, "email", default_value),
+        "phone": getattr(general_info, "phone", default_value),
+        "open_hours": getattr(general_info, "open_hours", default_value),
+        "video_url": getattr(general_info, "video_url", default_value),
+        "twitter_url": getattr(general_info, "twitter_url", default_value),
+        "github_url": getattr(general_info, 
+        "github_url", default_value),
+        "linkedin_url": getattr(general_info, "linkedin_url", default_value),
+
+        "services": services,
+    }
+
+    return render(request, "index.html", context)
