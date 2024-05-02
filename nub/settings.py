8cb7831c92ee1af965f7e7808ed0fbd2978a6a60
Diane Corriette <dcorriette@gmail.com>
2024-05-02 20:37:14 +0100

    Use collectstatic to update staticfiles

--- nub/settings.py
+++ nub/settings.py
@@ -133,3 +133,10 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.googlemail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'corriettediane@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') # app password
