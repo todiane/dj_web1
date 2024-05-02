87316cf57219696a11fb4edda6ff428922e07de1
Diane Corriette <dcorriette@gmail.com>
2024-05-02 20:45:23 +0100

    Install django-ckeditor and add to settings file

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from ckeditor.fields import RichTextField
 
 class GeneralInfo(models.Model):
     company_name = models.CharField(max_length=255, default="Company")
