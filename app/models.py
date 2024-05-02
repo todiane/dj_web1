0a554ba7fe9bbdd165c5481feb23f37cc28623b7
Diane Corriette <dcorriette@gmail.com>
2024-05-02 13:50:11 +0100

    Add two class models to app. Service and GeneralInfo then migrate

--- app/models.py
+++ app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class GeneralInfo(models.Model):
+    company_name = models.CharField(max_length=255, default="Company")
+    location = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    open_hours = models.CharField(max_length=100, blank=True, null=True)
+    video_url = models.URLField(blank=True, null=True)
+    twitter_url = models.URLField(blank=True, null=True)
+    github_url = models.URLField(blank=True, null=True)
+    linkedin_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.company_name
+    # class Meta:
+    #     db_table = "custom_table_name"
+
+class Service(models.Model):
+    icon = models.CharField(max_length=50, blank=True, null=True)
+    title = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
